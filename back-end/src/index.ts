7a4d1a00d31dfd41ad5f47c6f6b4a63507f6e016
Victor Govedice <victorgovedice@gmail.com>
2024-08-01 11:24:45 -0300

    👌 MELHORIA: created http post route

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,12 +1,28 @@
-// use this: https://www.tabnews.com.br/tiagoCali/back-end-com-typescript-para-iniciantes
-
-import express from 'express';
+// use this: https://abrir.link/RJpqe
+const express = require('express')
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.json())
+
+const data = [
+  {id: 1, nome: "John", email: 'john@gmail.com', senha: 'john@123'}
+]
+
+//rota de cadastro de usuario
+app.post('/cadastro', (req:any, res:any) => {
+    const newUser = req.body
+    data.push(newUser)
+    console.log(newUser)
+    res.end('usuario cadastrado')
+})
+
+app.get('/', (req: any, res: any) => {
+  res.json({
+    data,
+    message: 'database users'
+  })
 });
 
 app.listen(port, () => {
